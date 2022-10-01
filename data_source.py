fb38d089bab40d04911c0e3059d39137d445181d
HmZz <85006030+hmzz804@users.noreply.github.com>
2022-10-01 16:03:57 +0800

    Add files via upload

--- data_source.py
+++ data_source.py
@@ -2,6 +2,7 @@ import datetime
 import json
 import random
 from pathlib import Path
+from tabnanny import check
 from typing import Dict, List
 
 class Cave():
@@ -60,54 +61,75 @@ class Cave():
             json.dump(self.__cave, f, indent=4)
 
     def __check_path(self) -> bool:
+        '''
+        检查是否包含存储数据的路径
+        '''
         return self.data_path.exists() and self.data_path.is_file() \
             and self.cave_path.exists() and self.data_path.is_file()
 
-    def __check_A_id(self, id:str) -> bool:
+    def __check_wA_id(self, id:str) -> bool:
+        '''
+        检查白名单A中是否有指定id
+        '''
         return id in [i for i in self.__data["groups_id"][self.__group_id]["white_A"]]
 
-    def __check_B_id(self, id:str) -> bool:
+    def __check_wB_id(self, id:str) -> bool:
+        '''
+        检查白名单B中是否有指定id
+        '''
         return id in [i for i in self.__data["white_B"]]
+
+    def __check_set_id(self, id:int, change_state:int) -> bool:
+        '''
+        检查要操作的id的状态,并将传入的id的state改为change_state\\
+        state值的意义：
+            state 0 : 通过审核，可正常获取
+            state 1 : 待审核
+            state 2 : 未通过审核
+            state 3 : 被-r删除
+        '''
+        for i in self.__cave:
+            if i['cave_id'] == id:
+                if change_state != None : i['state'] = change_state
+                self.__save()
+                return i['state'] == 1
+        return False
 
     def __check_cd(self, cd:int, unit:str, last_time:str) -> list:
         '''
-        ## 检查cd情况  
+        检查cd情况\\
         参数：  
-        - `cd` : 冷却时间的数字
-        - `unit` : 冷却时间的单位
-        - `last_time` : 上次使用时间
-        
+            cd : 冷却时间的数字
+            unit : 冷却时间的单位
+            last_time : 上次使用时间
         返回列表：  
-        - 第0项(str):现在时间与上次时间的差值字符串
-        - 第1项(bool):cd是否已过
+            第0项(str):现在时间与上次时间的差值字符串
+            第1项(bool):cd是否已过
         '''
         now_time = datetime.datetime.now()
         last_time = datetime.datetime.strptime(last_time,"%Y-%m-%d %H:%M:%S.%f")
-        dif_sec = (now_time - last_time).seconds
-        dif_day = (now_time - last_time).days
-        result = []
+        dif_sec, dif_day = (now_time - last_time).seconds, (now_time - last_time).days
         if unit == "hour":
             sec_cd = cd*3600
-            result.append(str(round((sec_cd-dif_sec)/3600,2))+unit)
+            result = str(round((sec_cd-dif_sec)/3600,2))+unit
             if dif_sec >= sec_cd or dif_day > 0:
-                result.append(True)
+                return [result, True]
             else:
-                result.append(False)
+                return [result, False]
         elif unit == "min":
             sec_cd = cd*60
-            result.append(str(round((sec_cd-dif_sec)/60,1))+unit)
+            result = str(round((sec_cd-dif_sec)/60,1))+unit
             if dif_sec >= sec_cd or dif_day > 0:
-                result.append(True)
+                return [result, True]
             else:
-                result.append(False)
+                return [result, False]
         elif unit == "sec":
             sec_cd = cd
-            result.append(str(sec_cd-dif_sec)+unit)
+            result = str(sec_cd-dif_sec)+unit
             if dif_sec >= sec_cd or dif_day > 0:
-                result.append(True)
+                return [result, True]
             else:
-                result.append(False)
-        return result
+                return [result,False]
 
     def print_all(self):
         '''
@@ -161,7 +183,6 @@ class Cave():
         for i in self.__data["groups_dict"]:
             self.__data["groups_dict"][i]["m_list"].append({
                 'cave_id':cave_id,
-                'contributor_id':contributor_id,
                 'state':state})
         self.__cave.append({
                 'cave_id':cave_id,
@@ -218,45 +239,118 @@ class Cave():
         self.__save()
         return m_info
 
-    def A_add(self, a_id:str) -> dict:
-        if self.__check_A_id(id=a_id): return {'error':f'此账号已在群“{self.__group_id}”的白名单A中！'}
+
+    def wA_add(self, a_id:str) -> dict:
+        '''
+        添加白名单A成员
+            a_id : 添加的账号
+        返回结果
+        '''
+        if self.__check_wA_id(id=a_id): return {'error':f'此账号已在群“{self.__group_id}”的白名单A中！'}
         else:
             self.__data["groups_dict"][self.__group_id]["white_A"].append(a_id)
             self.__save()
             return {'success':f'成功将账号“{a_id}”添加到群“{self.__group_id}”的白名单A中！'}
 
-    def A_remove(self, r_id:str) -> dict:
-        if self.__check_A_id(id=r_id): 
+    def wA_remove(self, r_id:str) -> dict:
+        '''
+        删除白名单A成员
+            r_id : 删除的账号
+        返回结果
+        '''
+        if self.__check_wA_id(id=r_id): 
             self.__data["groups_dict"][self.__group_id]["white_A"].remove(r_id)
             self.__save()
             return {'success':f'成功将账号“{r_id}”移出群“{self.__group_id}”的白名单A！'}
         else: return {'error':f'账号“{r_id}”不在群“{self.__group_id}”的白名单A中！'}
 
-    def A_get(self) -> dict:
+    def wA_get(self) -> list:
+        '''
+        获取当前群的白名单A的成员列表
+        '''
         return self.__data["groups_dict"][self.__group_id]["white_A"]
 
-    def B_add(self, a_id) -> dict:
-        if self.__check_B_id(id=a_id): return {'error':f'此账号已在群“{self.__group_id}”的白名单B中！'}
+
+    def wB_add(self, a_id) -> dict:
+        '''
+        添加白名单B成员
+            a_id : 添加的账号
+        返回结果
+        '''
+        if self.__check_wB_id(id=a_id): return {'error':f'此账号已在群“{self.__group_id}”的白名单B中！'}
         else:
             self.__data["white_B"].append(a_id)
             self.__save()
             return {'success':f'成功将账号“{a_id}”添加到的白名单B中！'}
 
-    def B_remove(self, r_id) -> dict:
-        if  self.__check_B_id(id=r_id):
+    def wB_remove(self, r_id) -> dict:
+        '''
+        删除白名单B成员
+            r_id : 删除的账号
+        返回结果
+        '''
+        if  self.__check_wB_id(id=r_id):
             self.__data["white_B"].remove(r_id)
             self.__save()
             return {'success':f'成功将账号“{r_id}”移出白名单B！'}
         else: return {'error':f'账号“{r_id}”不在白名单B中'}
 
-    def B_get(self) -> dict:
+    def wB_get(self) -> list:
+        '''
+        获取当前的白名单B的成员列表
+        '''
         return self.__data["white_B"]
-        
 
-"""
-state
-    state 0 : 通过审核，可正常获取
-    state 1 : 待审核
-    state 2 : 未通过审核
-    state 3 : 被-r删除
-"""
+
+
+    def set_t(self, cave_id:int) -> dict:
+        '''
+        通过审核
+        '''
+        if self.__check_set_id(id = cave_id, change_state = 0):
+            for i in self.__data["groups_dict"]:
+                self.__data["groups_dict"][i]["m_list"].append({
+                    'cave_id':cave_id,
+                    'state':0})
+            self.__save()
+            return {'success':f'操作成功，序号:({cave_id})通过审核，加入回声洞。'}
+        else: return {'error':'此序号不存在或已被删除或已被审核！'}
+
+    def set_f(self, cave_id:int) -> dict:
+        '''
+        不通过审核
+        '''
+        if self.__check_set_id(id = cave_id, change_state = 2):
+            for i in self.__data["groups_dict"]:
+                self.__data["groups_dict"][i]["m_list"].append({
+                    'cave_id':cave_id,
+                    'state':2})
+            self.__save()
+            return {'success':f'操作成功，序号:({cave_id})通过不审核，已将其删除。'}
+        else: return {'error':'此序号不存在或已被删除或已被审核！'}
+
+    def set_l(self) -> list:
+        '''
+        返回当前未处理的审核序号列表
+        '''
+        msg = []
+        for i in self.__cave:
+            if i['state'] == 1:
+                msg.append(i)
+        return msg
+
+    def set_e(self, cave_id:int) -> dict:
+        '''
+        返回此id的审核情况\\
+        state值的意义：
+            state 0 : 通过审核，可正常获取
+            state 1 : 待审核
+            state 2 : 未通过审核
+            state 3 : 被-r删除
+        '''
+        if self.__check_set_id(id = cave_id, change_state = None):
+            for i in self.__cave:
+                if i['cave_id'] == cave_id:
+                    return {'success':i['state']}
+        else: return {'error':'此序号不存在或已被删除或已被审核'}
+
