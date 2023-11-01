f471062a9a2ad10480b279e6eaede18cad9be817
XieXiLin <support@xiexilin.com>
2023-11-01 20:17:39 +0800

    ✨ update: register commands

---
+++ nonebot_plugin_cave/__main__.py
@@ -0,0 +1,48 @@
+from nonebot.adapters import Event, Message
+from nonebot.matcher import Matcher
+from nonebot.params import CommandArg
+from nonebot.plugin import on_command
+
+# region command register
+# TODO: 使用更好的办法进行指令注册。
+cave_main = on_command("cave")
+cave_add = on_command("cave-a")
+cave_remove = on_command("cave-r")
+cave_set_cd = on_command("cave-c")
+cave_manage = on_command("cave-m")
+cave_whitelist = on_command("cave-w")
+cave_help = on_command("cave-h")
+cave_version = on_command("cave-v")
+# endregion
+
+@cave_main.handle()
+async def _(event: Event, matcher: Matcher):
+    pass
+
+@cave_add.handle()
+async def _(event: Event, matcher: Matcher, message: Message = CommandArg()):
+    pass
+
+@cave_remove.handle()
+async def _(event: Event, matcher: Matcher, message: Message = CommandArg()):
+    pass
+
+@cave_set_cd.handle()
+async def _(event: Event, matcher: Matcher, message: Message = CommandArg()):
+    pass
+
+@cave_manage.handle()
+async def _(event: Event, matcher: Matcher, message: Message = CommandArg()):
+    pass
+
+@cave_whitelist.handle()
+async def _(event: Event, matcher: Matcher, message: Message = CommandArg()):
+    pass
+
+@cave_help.handle()
+async def _(event: Event, matcher: Matcher):
+    pass
+
+@cave_version.handle()
+async def _(event: Event, matcher: Matcher):
+    pass
